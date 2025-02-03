286b0db3695ffe52f19e34dddc83f64ea1ab22f3
Abdu201213 <sabdu9049@gamil.com>
2025-02-03 22:19:43 +0300

    some change

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
